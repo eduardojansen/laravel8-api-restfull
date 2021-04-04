41aaa6d72c05ef15973b4dec53ea995dd155ce61
Eduardo Jansen <eduardo.j@hostdime.com.br>
2021-04-04 11:19:21 -0300

    Add fallback route in the API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Route;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->isJson()) {
+            return Route::respondWithRoute('api.fallback.404');
+        }
+
+        return parent::render($request, $exception);
     }
 }
